56cfacff3d5e152506e375b5da7dcf05caf780fa
Hasan TEKGÜL <htekgulds@gmail.com>
2016-03-06 20:51:35 +0200

    proje eklendi

---
+++ src/main/java/tr/gov/tuik/zk/telrehberi/controller/MainController.java
@@ -0,0 +1,12 @@
+package tr.gov.tuik.zk.telrehberi.controller;
+
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.select.SelectorComposer;
+
+/**
+ * Created by Hasan TEKGÜL
+ * on 6.03.2016.
+ */
+public class MainController extends SelectorComposer<Component> {
+
+}
